0fe7cd1f3f223460c5156fe4039b8eaed1d6c1fc
BasicDev <hayasuliman@macbook-air-alkhas-b-hayasuliman.local>
2016-02-19 15:57:42 +0300

    Initial Commit

---
+++ IAd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IAd
+//
+//  Created by  HayaSuliman on ١٠ جما١، ١٤٣٧ هـ.
+//  Copyright © ١٤٣٧ Omar'sGroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
