9a088fcdc839916cbfb8f992705ff021e2720d60
BasicDev <hayasuliman@MacBook-Air-alkhas-b-HayaSuliman.local>
2016-02-19 16:02:45 +0300

    IAds
    Create IAds banner and earn money

--- IAd/ViewController.swift
+++ IAd/ViewController.swift
@@ -6,20 +6,65 @@
 //  Copyright © ١٤٣٧ Omar'sGroup. All rights reserved.
 //
 
-import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
+import iAd
+
+
+
+
+
+var backgroundMusicPlayer:AVAudioPlayer = AVAudioPlayer()
+
+
+@IBOutlet var iadbannarView: ADBannerView?
+
+
+override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Release any cached data, images, etc that aren't in use.
+    
+    
+    self.canDisplayBannerAds = true
+    self.iadbannarView?.delegate = self
+    self.iadbannarView?.hidden = true
 }
+
+
+func bannerViewWillLoadAd(banner: ADBannerView!) {
+    
+}
+
+func bannerViewDidLoadAd(banner: ADBannerView!) {
+    
+    self.iadbannarView?.hidden = false
+    
+}
+
+func bannerViewActionDidFinish(banner: ADBannerView!) {
+    
+    
+    
+}
+func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
+    
+    return true
+}
+
+func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+    
+    self.iadbannarView?.hidden = true
+}
+
+override func prefersStatusBarHidden() -> Bool {
+    return true
+}
+}
+
+
+
+
+
+
+
 
